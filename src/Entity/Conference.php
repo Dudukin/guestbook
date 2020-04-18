e91c2d874f298167f4d917dea81aec0719f6dd89
Yehor Dudukin <edudukin2@gmail.com>
2020-04-18 15:08:33 +0300

    Lifecycles and creating new form

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -5,9 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ConferenceRepository")
+ * @UniqueEntity("slug")
  */
 class Conference
 {
@@ -38,6 +41,11 @@ class Conference
      */
     private $comments;
 
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    private $slug;
+
     public function __construct()
     {
         $this->comments = new ArrayCollection();
@@ -52,7 +60,12 @@ class Conference
     {
         return $this->id;
     }
-
+    public function computeSlug(SluggerInterface $slugger)
+    {
+        if (!$this->slug || '-' === $this->slug) {
+            $this->slug = (string) $slugger->slug((string) $this)->lower();
+        }
+    }
     public function getCity(): ?string
     {
         return $this->city;
@@ -116,6 +129,18 @@ class Conference
                 $comment->setConference(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }
